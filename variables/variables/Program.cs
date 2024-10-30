ceb6e93b9e0d146097521e3f8b71ab0c620a5e7d
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-10-30 19:36:51 -0400

    .

---
+++ variables/variables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x = 50;
+            int y = 50;
+            int z = 50;
+            Console.WriteLine(x + y + z);
+            Console.ReadKey();
+            */
+
+            /*
+            int x, y, z;
+            x = y = z = 50;
+            Console.WriteLine(x + y + z);
+            Console.ReadKey();
+            */
+
+        }
+    }
+}

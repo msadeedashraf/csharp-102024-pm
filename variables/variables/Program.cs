8a9a7357e6e754cd3cdc9d0197b27c0864c537b1
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-10-30 20:03:17 -0400

    .

--- variables/variables/Program.cs
+++ variables/variables/Program.cs
@@ -25,6 +25,59 @@ namespace variables
             Console.ReadKey();
             */
 
+
+            /*
+             int billAmount = 100;
+             int tipAmount = 15;
+            int totalBillAmount = billAmount+tipAmount;
+            Console.WriteLine(totalBillAmount);
+            Console.ReadKey();
+             */
+
+            /*
+             //Tip Calculator
+            Console.WriteLine("Tip Amount\n\n\n");
+            Console.WriteLine("##########\n\n\n");
+            Console.Write("Please input the Bill Amount: ");
+            string billAmount = Console.ReadLine();
+
+            Console.Write("Please input the Tip Amount: ");
+            string tipAmount = Console.ReadLine();
+
+
+            
+            int totalBillAmount = int.Parse(billAmount)  + Convert.ToInt32(tipAmount);
+
+            Console.WriteLine("Your Bill Total including the Tip is : " + totalBillAmount);
+            Console.ReadKey();
+            */
+
+
+            
+             //Loan Calculator
+            Console.WriteLine("Loan Calculator\n");
+            Console.WriteLine("##########\n");
+            Console.Write("Please input the Loan Amount: ");
+            string loanAmount = Console.ReadLine();
+
+            Console.Write("Please input the Rate %: ");
+            string rate = Console.ReadLine();
+
+
+            Console.Write("Please input the No. of Months: ");
+            string months = Console.ReadLine();
+
+
+
+
+            int monthlyPayment = 1000;
+
+            Console.WriteLine("Your Monthly Payment is : " + monthlyPayment);
+            Console.ReadKey();
+            
+
+
+
         }
     }
 }

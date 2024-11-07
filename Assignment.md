d5ad42576f40c5e1604d0b2097d26a099097fb3a
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-11-06 20:02:51 -0500

    .

---
+++ Assignment.md
@@ -0,0 +1,6 @@
+1. Create a 4 function Calculator ![link](/Assets/Four-Function-Calculator.png)
+2. Create a Program that can help you deposit/withdraw the amount from a Account Balance ![link](/Assets/Bank-Acount-Deposit-Withdrawal.png)
+3. Convert the user temperature from C to F and F to C;
+4. int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+Console.WriteLine("The temperature is " + celsius + " Celsius.");
---
+++ part2/part2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Random dice = new Random();
+
+            int roll1 = dice.Next();
+            int roll2 = dice.Next(101);
+            int roll3 = dice.Next(50, 101);
+
+            Console.WriteLine($"First roll: {roll1}");
+            Console.WriteLine($"Second roll: {roll2}");
+            Console.WriteLine($"Third roll: {roll3}");
+            */
+
+            int firstValue = 500;
+            int secondValue = 600;
+            int largerValue;
+            largerValue = Math.Max(firstValue, secondValue);
+
+            Console.WriteLine(largerValue);
+
+            Console.WriteLine( Math.Sqrt(5));
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}

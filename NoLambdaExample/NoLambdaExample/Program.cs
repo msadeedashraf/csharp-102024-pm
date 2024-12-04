7abb392530fa24ab48c3a0f915a9087f6a953e70
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-12-03 19:53:05 -0500

    .

---
+++ NoLambdaExample/NoLambdaExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoLambdaExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            
+            //Without Lambda Expression
+
+            List<int> numbers = new List<int> { 1,2,3,4,5,6,7,8,9,10};
+
+            List<int> evenNumbers = numbers.Where(IsEven).ToList();
+            List<int> oddNumbers = numbers.Where(IsOdd).ToList();
+
+            Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
+
+            Console.WriteLine("Even Numbers: " + string.Join(", ", oddNumbers));
+
+            
+
+
+            /*
+            foreach (int number in numbers) { 
+            Console.WriteLine(number);
+            }
+            */
+
+            Console.ReadLine();
+
+        }
+
+
+        static bool IsEven(int number)
+        { return number % 2 == 0;}
+
+        static bool IsOdd(int number)
+        { return number % 2 != 0; }
+
+
+
+
+
+    }
+}
---
+++ WithLambdaExpression/WithLambdaExpression/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithLambdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // A list of numbers
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // Use a lambda expression to find all even numbers
+            List<int> evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
+
+            // Use a lambda expression to find all odd numbers
+            List<int> oddNumbers = numbers.Where(n => n % 2 != 0).ToList();
+
+            // Display the results
+            Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
+            Console.WriteLine("Odd Numbers: " + string.Join(", ", oddNumbers));
+
+            Console.ReadLine();
+        }
+    }
+}
